dba90f56272fc9fb68c260a7456f34ad5eeafda3
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-19 15:21:00 -0300

    Lógica JS

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,36 +1,65 @@
 const state = {
-    view:{
-        squares: document.querySelectorAll(".square"),
-        enemy: document.querySelectorAll(".enemy"),
-        timeleft: document.querySelectorAll("#time-left"),
-        score: document.querySelectorAll("#score"),
+    view: {
+      squares: document.querySelectorAll(".square"),
+      enemy: document.querySelector(".enemy"),
+      timeLeft: document.querySelector("#time-left"),
+      score: document.querySelector("#score"),
     },
-    values:{
-        timerId: null,
-        gameVelocity: 1000,
+    values: {
+      gameVelocity: 1000,
+      hitPosition: 0,
+      result: 0,
+      curretTime: 60,
     },
-};
-
-function randomSquare() {
+    actions: {
+      timerId: setInterval(randomSquare, 1000),
+      countDownTimerId: setInterval(countDown, 1000),
+    },
+  };
+  
+  function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+  
+    if (state.values.curretTime <= 0) {
+      clearInterval(state.actions.countDownTimerId);
+      clearInterval(state.actions.timerId);
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+  }
+  
+  function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+  }
+  
+  function randomSquare() {
     state.view.squares.forEach((square) => {
-        square.classList.remove("enemy");
+      square.classList.remove("enemy");
     });
-
+  
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
-}
-
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
-
-function addListenerHitBox() {
-    state.view.squares.forEach((square) => {});
-};
-
-function initialize(){
-    moveEnemy();
-};
-
-initialize();
+    state.values.hitPosition = randomSquare.id;
+  }
+  
+  function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+      square.addEventListener("mousedown", () => {
+        if (square.id === state.values.hitPosition) {
+          state.values.result++;
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound("hit");
+        }
+      });
+    });
+  }
+  
+  function initialize() {
+    addListenerHitBox();
+  }
+  
+  initialize();
